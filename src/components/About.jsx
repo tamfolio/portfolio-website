2dd38e7c201b6b8ade8b5b143754f829c18ee38a
Tamilore <tamiloreolaniyi@gmail.com>
2023-05-04 10:15:39 +0100

    about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,23 +14,13 @@ const About = () => {
         </div>
 
         <p className="text-xl mt-20">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius,
-          deserunt illum mollitia officiis qui exercitationem perferendis neque
-          quasi a recusandae necessitatibus tempora iusto! Blanditiis error
-          iste, totam fugiat recusandae rerum laborum perferendis molestiae
-          aperiam asperiores nemo. Magni dolor maxime debitis vitae, eaque hic
-          ab mollitia voluptatibus, a nostrum eveniet laborum!
+        I am a software developer with 3 years Experience building user friendly Web applications and websites. My skills range from taking designs to development, ensuring that every component is pixel perfect and responsive across all devices, implementing complex functions, consuming data from api and working with databases
         </p>
 
         <br />
 
         <p className="text-xl">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum
-          pariatur, vel similique sint, nobis aspernatur ut praesentium
-          explicabo ipsam aliquid quasi laboriosam et culpa possimus repudiandae
-          quisquam ullam maiores ab unde. Fugiat odio mollitia nemo alias.
-          Commodi facilis atque nulla vero voluptatem explicabo. Quibusdam,
-          magni quo! Eum cupiditate debitis labore.
+          I am a very passionate and dedicated person, i enusre i put in my best in everything i do, i am a tem player, i have collaborated with team members both phyiscally and in remote situations. I understand user journey and user experience. my core strengths lie in my front end development.
         </p>
       </div>
     </div>
